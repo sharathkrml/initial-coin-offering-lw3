1d578847d43d22c583853469226ea67e64b659ee
sharath V <sharathkrml@gmail.com>
2022-03-23 22:29:23 +0530

    added connectWallet option

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,45 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { useEffect, useState, useRef } from "react";
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
 export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const connection = await web3ModalRef.current.connect();
+    const provider = new providers.Web3Provider(connection);
+    const { chainId } = await provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
+    }
+    if (needSigner) {
+      const signer = provider.getSigner();
+      return signer;
+    }
+    return provider;
+  };
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+
   return (
     <div>
       <Head>
@@ -14,7 +53,7 @@ export default function Home() {
           <div className={styles.description}>
             You can claim or mint Crypto Dev tokens here
           </div>
-          <button className={styles.button}>Connect your wallet</button>
+          <button onClick={connectWallet} className={styles.button}>Connect your wallet</button>
         </div>
         <div>
           <img className={styles.image} src="./0.svg" />
